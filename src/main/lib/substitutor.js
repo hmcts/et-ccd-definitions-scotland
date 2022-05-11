5a5028dfc8d04bb50a47f82da32b8b8b0319d5e6
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2022-05-11 09:38:36 +0100

    RET-1467 (#45)
    * RET-1467
    
    * Update ccd-config-base.xlsx

--- src/main/lib/substitutor.js
+++ src/main/lib/substitutor.js
@@ -1,9 +1,11 @@
-const ENVIRONMENT_VARIABLE_PREFIX = 'CCD_DEF';
+const ET_COS_URL = 'ET_COS_URL';
+const CCD_DEF_URL = 'CCD_DEF_URL';
 
 class Substitutor {
   static injectEnvironmentVariables (value) {
     Object.keys(process.env)
-      .filter(environmentVariableName => environmentVariableName.startsWith(ENVIRONMENT_VARIABLE_PREFIX))
+      .filter(environmentVariableName => environmentVariableName.startsWith(ET_COS_URL) ||
+          environmentVariableName.startsWith(CCD_DEF_URL))
       .forEach(environmentVariableName => {
         const environmentVariableValue = process.env[environmentVariableName];
         value = value.replace(new RegExp('\\$\\{' + environmentVariableName + '\\}', 'g'), environmentVariableValue);
