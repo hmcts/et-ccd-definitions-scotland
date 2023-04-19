d4fe33a3bda9910f1cdf6f2253f18085c481128d
Daniel Furnivall <danielfurnivall@gmail.com>
2023-04-19 23:14:12 +0100

    moved config to external json

---
+++ json-env.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const env = process.argv[2];
+if (!env) {
+  console.error('Provide an environment parameter');
+  process.exit(1);
+}
+
+const config = JSON.parse(fs.readFileSync('env.json', 'utf-8'));
+
+if (!config[env]) {
+  console.error(`Environment "${env}" not found in env.json`);
+  process.exit(1);
+}
+
+Object.entries(config[env]).forEach(([k, v]) => {
+  process.env[k] = v;
+});
+
+process.env.ET_ENV = env;
+
+require('./node_modules/.bin/yarn')([
+  'run',
+  `generate-excel`,
+  '-e',
+  '*-prod.json',
+]);
+
